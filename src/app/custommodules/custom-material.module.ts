a569e6d3a2c7d734dd02c3039cbaf484693d6180
Dheerendra.PANDEY-ext <Dheerendra.PANDEY-ext@aldautomotive.com>
2021-05-11 09:13:59 +0530

    Angular Version upgrade + tsconfig and package update

--- src/app/custommodules/custom-material.module.ts
+++ src/app/custommodules/custom-material.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import {
-  MatToolbarModule,
-  MatTabsModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatSelectModule,
-  MatButtonModule,
-  MatAutocompleteModule
-} from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
   imports: [
